74a40db75c8fcd811f703d904c1058a96c594b90
Matthew O'Connor <oconnormattc@gmail.com>
2026-01-28 14:03:10 -0500

    Index updated with a couple routes. More will be added shortly to have routing functionality complete

--- website_frontend/src/index.js
+++ website_frontend/src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router';
+import { BrowserRouter, Routes, Route } from 'react-router';
+
+import Home from 'pages/Home';
+import Games from 'pages/Games';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/Games' element={<Games />} />
+
+    </Routes>
   </BrowserRouter>
   // </React.StrictMode>
 );
