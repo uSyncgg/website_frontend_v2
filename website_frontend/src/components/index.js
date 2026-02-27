aa5a12cf40db549fd1d5fb30e353097694fb93eb
Matthew O'Connor <oconnormattc@gmail.com>
2026-02-27 01:14:02 -0500

    Updating index exports

--- website_frontend/src/components/index.js
+++ website_frontend/src/components/index.js
@@ -4,8 +4,15 @@ import Navbar from "./Navbar/Navbar";
 import Footer from "./Footer/Footer";
 import { HostBanner } from "./HostBanner/HostBanner";
 import MapCoverHome from "./MapCover/MapCoverHome";
+import MapCoverGame from "./MapCover/MapCoverGame";
 import { LoadingWheel } from "./LoadingWheel/LoadingWheel";
 import { FormDataCheck } from "./FormDataCheck/FormDataCheck";
+import { GameImage } from "./GameImage/GameImage";
+import { HeaderImage } from "./HeaderImage/HeaderImage";
+import { EventTypeImages } from "./EventTypeImages/EventTypeImages";
+import MoreInfoButton from "./MoreInfoButton/MoreInfoButton";
+import { EmailIcon } from "./EmailIcon/EmailIcon";
+import { TeamPhoto } from "./TeamPhoto/TeamPhoto";
 
 // Payment Form
 import { PaymentForm } from "./PaymentForm/PaymentForm";
@@ -27,4 +34,11 @@ export {
     LoadingWheel,
     FormDataCheck,
     Receipt,
+    GameImage,
+    HeaderImage,
+    EventTypeImages,
+    MapCoverGame,
+    MoreInfoButton,
+    EmailIcon,
+    TeamPhoto
 };
