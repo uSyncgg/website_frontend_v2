796faf79476a52a59c44871a2e695160f5e6a7d8
Matthew O'Connor <oconnormattc@gmail.com>
2026-04-21 01:53:08 -0400

    Fixing Navbar individual item click and header image text scaling

--- website_frontend/src/components/Navbar/SingleNavItems.jsx
+++ website_frontend/src/components/Navbar/SingleNavItems.jsx
@@ -1,7 +1,7 @@
 import { NavLink, useMatch } from "react-router";
 import styles from './Navbar.module.css'
 
-export const GamesNavItem = () => {
+export const GamesNavItem = ({ closeMenu }) => {
   const gamesMatch = useMatch("/games");
   const specificGameMatch = useMatch("/games/:game/*");
   const lansMatch = useMatch("/games/:game/lans/*");
@@ -9,24 +9,36 @@ export const GamesNavItem = () => {
 
   const isActive = (gamesMatch || specificGameMatch) && !lansMatch && !leaguesMatch;
 
+  const handleClick = (e) => {
+    if (window.innerWidth <= 768) {
+      closeMenu();
+    }
+  }
+
   return (
     <div className={styles.navItem}>
-      <NavLink to="/games" className={`${styles.navLink} ${isActive ? styles.activeParent : ''}`}>
+      <NavLink to="/games" className={`${styles.navLink} ${isActive ? styles.activeParent : ''}`} onClick={handleClick}>
         Games
       </NavLink>
     </div>
   );
 }
 
-export const TournamentNavItem = () => {
+export const TournamentNavItem = ({ closeMenu }) => {
   const tourneyMatch = useMatch("/tournaments");
   const generalMatch = useMatch("/tournaments/*");
 
   const isActive = tourneyMatch || generalMatch;
 
+  const handleClick = (e) => {
+    if (window.innerWidth <= 768) {
+      closeMenu();
+    }
+  }
+
   return (
     <div className={styles.navItem}>
-      <NavLink to="/tournaments" className={`${styles.navLink} ${isActive ? styles.activeParent : ''}`}>
+      <NavLink to="/tournaments" className={`${styles.navLink} ${isActive ? styles.activeParent : ''}`} onClick={handleClick}>
         Tournaments
       </NavLink>
     </div>
