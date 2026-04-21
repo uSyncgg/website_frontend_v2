c5a5f615551bbcd90213a4242a5978ab8fa626c8
Matthew O'Connor <oconnormattc@gmail.com>
2026-04-21 00:29:29 -0400

    Updating Header Images and their CSS elements - still W.I.P

--- website_frontend/src/components/HeaderImage/HeaderImage.jsx
+++ website_frontend/src/components/HeaderImage/HeaderImage.jsx
@@ -2,15 +2,33 @@ import styles from './HeaderImage.module.css';
 import gameStyles from '../GameImage/GameImage.module.css';
 import './HeaderImageStandard.css'
 import React from 'react';
+import { Link } from 'react-router';
 
-export const HeaderImage = ({ title, imageClass, verification }) => {
-
+export const HeaderImage = ({ title, imageClass, verification, verified=false, location="" }) => {
+    console.log(verified);
+    console.log(imageClass)
     return (
         <div className={`${styles.headerImage} ${imageClass} ${title === undefined ? styles.noTitle : ""}`}>
-            {title !== undefined && verification === undefined &&
+            {title !== undefined && verification === undefined && verified === false &&
                 <div className={styles.headerImageTitle}>
+                    
+
                     <h2 className={gameStyles.white}>{title}</h2>
                     <img className="underlineImg" src="https://i.imgur.com/eNhKhTI.png" alt="underline" />
+
+                    
+                </div>
+            }
+
+            {title !== undefined && verification === undefined && verified === true &&
+                <div className={styles.headerImageTitle}>
+                    {location !== "" && 
+                        <div className={styles.location}>
+                            <p className={styles.locationText}>{location}</p>
+                        </div>
+                    }
+
+                    <h2>{title}</h2>
                 </div>
             }
 
