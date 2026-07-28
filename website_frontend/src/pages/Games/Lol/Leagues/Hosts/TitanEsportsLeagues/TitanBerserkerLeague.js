e00437bca87c0841eef3cd6c1404856b6e1b3835
Matthew O'Connor <oconnormattc@gmail.com>
2026-07-27 21:42:51 -0400

    Add Titan Berserker and Sentinel Leagues
    - New pages for Titan Berserker League ($50/team, Emerald 4 Avg) and
      Titan Sentinel League ($100/team, M100 Avg)
    - Added banners for both leagues to the Titan Esports Leagues listing
    - Registered routes in LolRoutes.js
    - Added redirects and sitemap entries for both new pages

---
+++ website_frontend/src/pages/Games/Lol/Leagues/Hosts/TitanEsportsLeagues/TitanBerserkerLeague.js
@@ -0,0 +1,37 @@
+import { SeoData, EventInfoCard, HeaderImage, ExternalButton, BackButton, VerifiedText } from "components";
+import '../../../../EventInformation.css';
+
+export const TitanBerserkerLeague = () => {
+    return (
+        <div className="standardContainer">
+            <SeoData
+                title={"Titan Berserker League - League of Legends"}
+                description="Titan Berserker League is a League of Legends league with an Emerald 4 average. Do you have what it takes to win the entire league?"
+                canonicalPath={"/games/LoL/leagues/titan-leagues/berserker"}
+            />
+            <HeaderImage imageClass={"titanLOLLeagues"} />
+
+            <div className="verifiedContainer">
+                <VerifiedText />
+            </div>
+
+            <div className="eventInfoCardContainer">
+                <div>
+                    <EventInfoCard title={"Date"} infoList={["Annual Summer, Fall, and Winter Seasons"]} regionTitle={"Region/Type"} regionInfoList={["5v5 - NA"]} />
+                </div>
+
+                <div>
+                    <EventInfoCard title={"Details"} infoList={["1st Place = 35% - 2nd Place = 15% - 3rd/4th Place = 5% - Operators = 40%", "Support on Discord", "Live Streamed Matches"]} />
+                </div>
+
+                <div>
+                    <EventInfoCard title={"Entry Fee"} infoList={["$50 per Team", "Forfeit Deposit TBD", "Emerald 4 Avg"]} footer={<ExternalButton host={"Titan Berserker League"} blank={true} title={"Join Now"} path={"https://lol.titanesports.org/"} />}/>
+                </div>
+            </div>
+
+            <div className="backButtonContainer">
+                <BackButton path={"/games/LoL/leagues/titan-leagues"} />
+            </div>
+        </div>
+    );
+}
