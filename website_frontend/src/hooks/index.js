ba4dd4f463991cc5b26fcfee9deb1f9f021e9066
Matthew O'Connor <85258652+MOconnorUS@users.noreply.github.com>
2026-09-11 20:36:15 -0400

    Creating and integrating the payment portal for E-Day LAN and configuring all the routes. (#98)

--- website_frontend/src/hooks/index.js
+++ website_frontend/src/hooks/index.js
@@ -1,5 +1,6 @@
 import useCheckResize from "./CheckResize/useCheckResize";
 import { useLeagueEvents, useLanEvents, useAllLanEvents, useWagerEvents, useXpEvents, useEvent, useEventByPath, useLanInfo, useLeagueInfo, useLeagueChildren, useVerifiedEvents, useAllLans } from "./useEvents";
+import { useEventPasses, useEventReceipt } from "./useEventRegistration";
 
 export {
     useCheckResize,
@@ -14,5 +15,7 @@ export {
     useLeagueInfo,
     useLeagueChildren,
     useVerifiedEvents,
-    useAllLans
+    useAllLans,
+    useEventPasses,
+    useEventReceipt
 }
