c3a1591d68482b9331dfddf53ae058a129845501
Matthew O'Connor <oconnormattc@gmail.com>
2026-09-13 15:35:37 -0400

    Updating all lans bubble.

--- website_frontend/src/components/EventTypeImages/EventTypeImages.jsx
+++ website_frontend/src/components/EventTypeImages/EventTypeImages.jsx
@@ -2,6 +2,7 @@ import styles from './EventTypeImages.module.css';
 import { Link } from 'react-router';
 import React from 'react';
 import onlineTournamentsBubble from 'assets/images/coming_soon/Online Tournaments Coming Soon Bubble (9.13.26).webp';
+import allLansBubble from 'assets/images/misc/all lans bubble.webp';
 
 const ALL_ARTICLES = [
     { path: "/more/articles/The-Problem-With-Esports",             img: "https://i.imgur.com/vBqSUhI.png", alt: "The Problem With Esports" },
@@ -200,7 +201,7 @@ export const EventTypeImages = ({ page, articleName='' }) => {
             {page === "All LANs" &&
                 <div className={`${styles.imageEventContainer} ${styles.soloImage}`}>
                     <Link to="/lans/all">
-                        <img src="https://i.imgur.com/OPhy6nX.png" alt='All LANs'/>
+                        <img src={allLansBubble} alt='All LANs'/>
                     </Link>
                 </div>
             }
